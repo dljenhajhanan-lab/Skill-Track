c343b0ba755c019271b81716796125bf8d8f3e08
dljenhajhanan-lab <dljenhajhanan@gmail.com>
2025-10-28 22:37:37 +0300

    Create Profile for Company and Professor

---
+++ controllers/profile/company.js
@@ -0,0 +1,18 @@
+import { catchAsync } from "../../utils/catchAsync.js";
+import { successResponse } from "../../utils/responseHandler.js";
+import { createCompanyProfile, getCompanyProfile, updateCompanyProfile } from "../../services/companyService.js";
+
+export const createCompany = catchAsync(async (req, res) => {
+  const result = await createCompanyProfile(req.user._id, req.body);
+  successResponse(res, result.data, result.message, 201);
+});
+
+export const getCompany = catchAsync(async (req, res) => {
+  const result = await getCompanyProfile(req.user._id);
+  successResponse(res, result.data, result.message, 200);
+});
+
+export const updateCompany = catchAsync(async (req, res) => {
+  const result = await updateCompanyProfile(req.user._id, req.body);
+  successResponse(res, result.data, result.message, 200);
+});
