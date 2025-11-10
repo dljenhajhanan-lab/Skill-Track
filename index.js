04da02ade90610895cf7941a9e04e06831c8b1d4
dljenhajhanan-lab <dljenhajhanan@gmail.com>
2025-11-10 13:15:51 +0300

    Fix Port Error in Index.js File

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./config/db.js" 
+import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
-import profileRoutes from "./routes/profile.js"
-import otpRouter from "./routes/otpRoutes.js"
+import profileRoutes from "./routes/profile.js";
+import otpRouter from "./routes/otpRoutes.js";
 import courseraRoutes from "./routes/coursera.js";
-import approvalRoutes from "./routes/approvalRoutes.js"
+import approvalRoutes from "./routes/approvalRoutes.js";
 import badgeRoutes from "./routes/badgeRoutes.js";
 import skillRoutes from "./routes/skillRoutes.js";
-import { errorHandler } from "./middleware/errorHandler.js";
 import followRoutes from "./routes/followroutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
 import reactionRoutes from "./routes/reactionRoutes.js";
 
+import { errorHandler } from "./middleware/errorHandler.js";
+
 dotenv.config();
 connectDB();
-
-const port = process.env.PORT || 3000;
-app.listen;
 
 const app = express();
 app.use(cors());
@@ -27,12 +26,12 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/approval", approvalRoutes);
-app.use("/api/profile",profileRoutes)
-app.use("/api/otp",otpRouter)
+app.use("/api/profile", profileRoutes);
+app.use("/api/otp", otpRouter);
 app.use("/api/coursera", courseraRoutes);
 app.use("/api/badges", badgeRoutes);
 app.use("/api/skills", skillRoutes);
-app.use("/api/follow",followRoutes);
+app.use("/api/follow", followRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/reactions", reactionRoutes);
@@ -40,4 +39,4 @@ app.use("/api/reactions", reactionRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT);
