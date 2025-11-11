f9b19ab788d21b54baec8879c6b2a3d7394d8bc2
dljenhajhanan-lab <dljenhajhanan@gmail.com>
2025-11-11 10:56:15 +0300

    Use ValidateRequest in CommuintyRouts

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
 import { createPostController, getAllPostsController, getPostByIdController, updatePostController, deletePostController } from "../controllers/community/postController.js";
-
+import { validateRequest } from "../middleware/validateRequest.js";
 const router = express.Router();
 
-router.post("/create", protect, createPostController);
+router.post("/create", protect,validateRequest, createPostController);
 router.get("/getPosts", protect, getAllPostsController);
 router.get("/getPostDetail/:id", protect, getPostByIdController);
-router.put("/updatePost/:id", protect, updatePostController);
+router.put("/updatePost/:id", protect,validateRequest, updatePostController);
 router.delete("/deletePost/:id", protect, deletePostController);
 
 export default router;
